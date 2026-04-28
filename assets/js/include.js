0ee0c8de5ce5a899acd4667289c3792c83b314a4
HiVaRiX <hivarixglobal@gmail.com>
2026-04-28 19:27:10 +0100

    Update include.js

--- assets/js/include.js
+++ assets/js/include.js
@@ -1,13 +1,2 @@
-function loadHTML(id, file) {
-    fetch(file)
-        .then(res => res.text())
-        .then(data => {
-            document.getElementById(id).innerHTML = data;
-        })
-        .catch(err => console.error("Include error:", err));
-}
-
-document.addEventListener("DOMContentLoaded", () => {
-    loadHTML("header", "/assets/components/header.html");
-    loadHTML("footer", "/assets/components/footer.html");
-});
+loadHTML("header", "/assets/header.html");
+loadHTML("footer", "/assets/footer.html");
